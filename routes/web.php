bcded4b4ab89003d63db55320e4fa6ef4420ad2f
RejwanShafi25 <rejstudy@gmail.com>
2025-01-26 08:03:00 +0600

    Delete all files from google_auth

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/redirect', 'GoogleAuthController@redirect')->name('google.redirect');
-Route::get('/callback/google', 'GoogleAuthController@callbackGoogle')->name('google.callback');
